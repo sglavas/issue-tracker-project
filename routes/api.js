e8c0e0e095055e1772593f3fcc0f56381e67ffc7
sglavas <sglavas@m.ffzg.hr>
2025-11-02 01:50:00 +0100

    feat: get data from input and query parameters

--- routes/api.js
+++ routes/api.js
@@ -6,22 +6,28 @@ module.exports = function (app) {
   
     .get(function (req, res){
       let project = req.params.project;
-      
+      // Get query parameters from the GET request
+      const { assigned_to, status_text, _id, open, issue_title, issue_text, created_by, created_on, updated_on } = req.query;
+
     })
     
     .post(function (req, res){
       let project = req.params.project;
-      
+      // Get values from the create input
+      const { issue_title, issue_text, created_by, assigned_to, status_text } = req.body;
     })
     
     .put(function (req, res){
       let project = req.params.project;
-      
+      // Get values from the update input
+      const { _id, issue_title, issue_text, created_by, assigned_to, status_text } = req.body;
+
     })
     
     .delete(function (req, res){
       let project = req.params.project;
-      
+      // Get ID from the delete input
+      const { _id } = req.body;
     });
     
 };
