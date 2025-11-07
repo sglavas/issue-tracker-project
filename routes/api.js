97a61f56ed28f165703902527df3f5181880d6ff
sglavas <sglavas@m.ffzg.hr>
2025-11-07 23:41:01 +0100

    feat(api): implement findExercise function
    - Import findExercise function
    - Add query parameters and project to documentObject
    - Call findExercise with document object as argument on /api/issues/:project GET method

--- routes/api.js
+++ routes/api.js
@@ -1,22 +1,35 @@
 'use strict';
 
-const { createAndSaveIssue } = require('./../database/mongoDB')
+const { createAndSaveIssue, findExercise } = require('./../database/mongoDB')
 
 module.exports = function (app) {
 
   app.route('/api/issues/:project')
   
-    .get(function (req, res){
+    .get(async function (req, res){
       let project = req.params.project;
       // Get query parameters from the GET request
       const { assigned_to, status_text, _id, open, issue_title, issue_text, created_by, created_on, updated_on } = req.query;
 
+      // Add project to the document object
+      let documentObject = req.query;
+      documentObject.project = project;
+
+      // Query the DB with the document object
+      let result = await findExercise(documentObject);
+
+      res.json(result);
     })
     
     .post(function (req, res){
       let project = req.params.project;
 
-      createAndSaveIssue(req.body)
+      // Add project to the document object
+      let documentObject = req.body;
+      documentObject.project = project
+
+      // Query the DB with the document object
+      createAndSaveIssue(documentObject)
 
     })
     
